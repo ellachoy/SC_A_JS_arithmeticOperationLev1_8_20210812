51bd7b4933f9ca7c1f005d9cedbab057d0947274
Ella Choy <egal21de@gmail.com>
2021-08-12 19:06:04 +0200

    ArithmeticO

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+console.log("hi arithmetic operators")
+document.write("Übung lev1_8: arithmetic operator")
+
+
+// Aufgabenstellung 1
+// Deklariere die Variablen x = 20, y = 30.
+let x=20;
+let y=30;
+console.log(x);
+
+// Addiere x und y und gib das Ergebnis in der Konsole aus.
+console.log(x + y);
+
+// Subtrahiere y von x und zeige das Ergebnis in der Konsole. Dann subtrahiere x von y und gib das Ergebnis in der Konsole aus.
+console.log(y-x);
+console.log(x-y);
+
+// Multipliziere x und y und gib das Ergebnis der Konsole aus.
+console.log(x*y);
+
+// Teile x durch y und gib das Ergebnis mit der Konsole aus.
+console.log(x/y);
+
+// Deklariere eine weitere Variable z mit dem Wert 10. Multipliziere x und y. Teile dann das Ergebnis durch z. Speichere das Ergebnis in einer neuen Variablen namens resultOne.
+let z =10;
+resultOne=x*y/z;
+console.log(resultOne);
+
+
+// Aufgabenstellung 2
+
+// Deklariere zwei Variablen a mit dem Wert 15 und b mit dem Wert 9. Zeige den Rest an, wenn a durch b geteilt wird. Nutze Remainder(%).
+
+let a=15;
+let b=9;
+console.log(a%b);
+// Deklariere eine weitere Variable c mit dem Wert 20. Addiere a und b und multipliziere das Ergebnis mit c. Speichere das Ergebnis in der Variable resultTwo. Gib resultTwo in der Konsole aus.
+c=20;
+resultTwo=(a+b)*c;
+console.log(resultTwo);
+// Inkrementiere a. Gib das Ergebnis in der Konsole aus.
+// a++;
+console.log(a+1);
+
+// Dekrementiert b. Gib das Ergebnis in der Konsole aus.
+// b--
+console.log(b-1);
+// Subtrahiere b von a und speichere das Ergebnis in einer neuen Variablen resultThree.
+resultThree=b-a;
+console.log(resultThree);
+// Gib den Rest aus wenn resultOne durch resultTwo geteilt wird in der Konsole aus.  Nutze Remainder(%).
+console.log(resultOne%resultTwo);
+
